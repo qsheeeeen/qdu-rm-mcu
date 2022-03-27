848973bbc4971ea2c95d9714139226c803d5d2c7
Qu Shen <503578404@qq.com>
2022-03-27 15:58:49 +0800

    修复缺失的BSP UART回调函数

--- src/hal/stm32f4xx_it.c
+++ src/hal/stm32f4xx_it.c
@@ -317,6 +317,7 @@ void USART1_IRQHandler(void) {
   /* USER CODE END USART1_IRQn 0 */
   HAL_UART_IRQHandler(&huart1);
   /* USER CODE BEGIN USART1_IRQn 1 */
+  BSP_UART_IRQHandler(&huart1);
 
   /* USER CODE END USART1_IRQn 1 */
 }
@@ -447,6 +448,7 @@ void USART6_IRQHandler(void) {
   /* USER CODE END USART6_IRQn 0 */
   HAL_UART_IRQHandler(&huart6);
   /* USER CODE BEGIN USART6_IRQn 1 */
+  BSP_UART_IRQHandler(&huart6);
 
   /* USER CODE END USART6_IRQn 1 */
 }
